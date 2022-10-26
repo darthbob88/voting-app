e224eaa8a8d9aef0512aa3f0f59b7b4abdcfba12
Raymond Price <darthbob88@users.noreply.github.com>
2022-10-26 11:16:50 -0700

    Created test values for ballot and poll.

--- src/model/DataAccess.test.ts
+++ src/model/DataAccess.test.ts
@@ -1,5 +1,5 @@
 import { castBallot, createPoll, retrievePoll, retrievePollList } from "./DataAccess";
-import { Ballot } from "./Poll";
+import { testBallot, testPoll } from "./Poll";
 
 // TODO: Remove this once I setup proper data storage.
 beforeEach(() => {
@@ -8,89 +8,53 @@ beforeEach(() => {
 
 describe("Data Access Layer", () => {
     test("Creating new poll should return poll ID", () => {
-        const newPoll = {
-            ID: "pollID",
-            name: "pollName",
-            creator: "pollCreator",
-            creation: Date.now(),
-            expiration: Date.now(),
-            method: "FPTP",
-            candidates: ["apple", "orange", "banana"],
-            ballots: []
-        };
-        const result = createPoll(newPoll);
-        expect(result).toEqual(newPoll.ID);
+
+        const result = createPoll(testPoll);
+        expect(result).toEqual(testPoll.ID);
     })
 
     test("Creating new poll should update list of poll IDs", () => {
-        const newPoll = {
-            ID: "pollID",
-            name: "pollName",
-            creator: "pollCreator",
-            creation: Date.now(),
-            expiration: Date.now(),
-            method: "FPTP",
-            candidates: ["apple", "orange", "banana"],
-            ballots: []
-        };
-        createPoll(newPoll);
+
+        createPoll(testPoll);
 
         const result = retrievePollList();
-        expect(result).toContain(newPoll.ID);
+        expect(result).toContain(testPoll.ID);
     })
 
     test("Creating new poll should throw error if existing poll has same ID", () => {
-        const newPoll = {
-            ID: "pollID",
-            name: "pollName",
-            creator: "pollCreator",
-            creation: Date.now(),
-            expiration: Date.now(),
-            method: "FPTP",
-            candidates: ["apple", "orange", "banana"],
-            ballots: []
-        };
-        createPoll(newPoll);
 
-        expect(() => createPoll(newPoll)).toThrow()
+        createPoll(testPoll);
+
+        expect(() => createPoll(testPoll)).toThrow()
     })
 
     test("Creating and retrieving new poll should return the same object", () => {
-        const newPoll = {
-            ID: "pollID",
-            name: "pollName",
-            creator: "pollCreator",
-            creation: Date.now(),
-            expiration: Date.now(),
-            method: "FPTP",
-            candidates: ["apple", "orange", "banana"],
-            ballots: []
-        };
-        createPoll(newPoll);
-        const result = retrievePoll(newPoll.ID);
-        expect(result).toEqual(newPoll);
+
+        createPoll(testPoll);
+        const result = retrievePoll(testPoll.ID);
+        expect(result).toEqual(testPoll);
     })
 
     test("Casting ballot should update the poll", () => {
-        const newPoll = {
-            ID: "pollID",
-            name: "pollName",
-            creator: "pollCreator",
-            creation: Date.now(),
-            expiration: Date.now(),
-            method: "FPTP",
-            candidates: ["apple", "orange", "banana"],
-            ballots: []
-        };
-        createPoll(newPoll);
-        let result = retrievePoll(newPoll.ID);
+
+        createPoll(testPoll);
+        let result = retrievePoll(testPoll.ID);
         expect(result?.ballots).toHaveLength(0);
 
-        const newBallot: Ballot = { ballotID: "ballotID", voterID: "voterID", pollID: "pollID", votes: [1, 2, 3, 4] }
-        castBallot(newBallot, newPoll.ID);
-        result = retrievePoll(newPoll.ID);
+        castBallot(testBallot, testPoll.ID);
+        result = retrievePoll(testPoll.ID);
         expect(result?.ballots).toHaveLength(1);
-        expect(result?.ballots).toContainEqual(newBallot);
+        expect(result?.ballots).toContainEqual(testBallot);
+
+    })
+
+    xtest("Should not be able to cast ballot after poll expiration", () => {
+        createPoll(testPoll);
+        let result = retrievePoll(testPoll.ID);
+        expect(result?.ballots).toHaveLength(0);
+
+        const expiredBallot = { ...testBallot, timestamp: Date.now() };
+        expect(() => castBallot(expiredBallot, testPoll.ID)).toThrow();
 
     })
 })
--- src/model/Poll.ts
+++ src/model/Poll.ts
@@ -9,9 +9,23 @@ export type Poll = {
     ballots: Ballot[]
 }
 
+export const testPoll: Poll = {
+    ID: "pollID",
+    name: "pollName",
+    creator: "pollCreator",
+    creation: Date.now(),
+    expiration: Date.now(),
+    method: "FPTP",
+    candidates: ["apple", "orange", "banana"],
+    ballots: []
+};
+
 export type Ballot = {
+    timestamp: number,
     ballotID: string,
     voterID: string,
     pollID: string,
     votes: any,
 }
+
+export const testBallot: Ballot = { timestamp: Date.now(), ballotID: "ballotID", voterID: "voterID", pollID: "pollID", votes: [1, 2, 3, 4] }
