c0fbbe1adad4f7105f8860fb1bb496d3e12ffe02
Raymond Price <darthbob88@gmail.com>
2023-04-01 20:48:01 -0700

    Adding initial poll for testing purposes.

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,64 @@ import NewPoll from './components/NewPoll/NewPoll';
 import PollListing from './components/PollListing/PollListing';
 
 function App() {
+  localStorage.setItem("best-fruit", JSON.stringify({
+    "ID": "best-fruit",
+    "name": "pollName",
+    "creator": "darthbob88@gmail.com",
+    "creation": 1668465233437,
+    "expiration": 1668551633437,
+    "method": "FPTP",
+    "candidates": [
+      "apple",
+      "orange",
+      "banana"
+    ],
+    "ballots": [
+      {
+        "ballotID": "butts",
+        "votes": "apple",
+        "timestamp": 1670282960239,
+        "pollID": "best-fruit",
+        "voterID": "butts"
+      },
+      {
+        "ballotID": "butts",
+        "voterID": "butts",
+        "votes": "orange",
+        "timestamp": 1671854055125,
+        "pollID": "best-fruit"
+      },
+      {
+        "ballotID": "butts",
+        "voterID": "butts",
+        "votes": "mango",
+        "timestamp": 1671854347738,
+        "pollID": "best-fruit"
+      },
+      {
+        "ballotID": "butts",
+        "voterID": "butts",
+        "votes": "mango",
+        "timestamp": 1671854350413,
+        "pollID": "best-fruit"
+      },
+      {
+        "ballotID": "butts",
+        "voterID": "butts",
+        "votes": "orange",
+        "timestamp": 1671854352933,
+        "pollID": "best-fruit"
+      },
+      {
+        "ballotID": "butts",
+        "voterID": "butts",
+        "votes": "mango",
+        "timestamp": 1671854367541,
+        "pollID": "best-fruit"
+      }
+    ]
+  }));
+
   return (
     <div className="App">
       <header className="App-header">
